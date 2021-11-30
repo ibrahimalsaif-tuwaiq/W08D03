933f26fc8f96b2551dbe0536d25945db07426b57
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-30 13:33:33 +0300

    signup controller

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -3,8 +3,32 @@ const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// Config .env file
+dotenv.config();
+
+// Get SALT variable from .env
+const SALT = Number(process.env.SALT);
+
 const signup = async (req, res) => {
-  // code
+  const { email, password, role } = req.body;
+
+  const lowerCaseEmail = email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(password, SALT);
+
+  const newUser = new usersModel({
+    email: lowerCaseEmail,
+    password: hashedPassword,
+    role,
+  });
+
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 const login = (req, res) => {
