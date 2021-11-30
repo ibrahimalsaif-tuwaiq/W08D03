e2e0c87a3168286354ca27155a4e7189d444ee02
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-30 15:15:39 +0300

    get deleted todos controller & fixing add todo controller

--- routers/controllers/tasks.js
+++ routers/controllers/tasks.js
@@ -20,7 +20,22 @@ const getTodos = (req, res) => {
 };
 
 const getDeletedTodos = (req, res) => {
-  // code
+  if (!req.token.deleted) {
+    tasksModel
+      .find({ creator: req.token.id, deleted: true })
+      .then((result) => {
+        if (result.length > 0) {
+          res.status(200).json(result);
+        } else {
+          res.status(404).json({ message: "There is no deleted todos!!" });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(404).json({ message: "This user is deleted" });
+  }
 };
 
 const getTodo = (req, res) => {
@@ -28,21 +43,25 @@ const getTodo = (req, res) => {
 };
 
 const addTodo = (req, res) => {
-  const { name } = req.body;
+  if (!req.token.deleted) {
+    const { name } = req.body;
 
-  const newTodo = new tasksModel({
-    name,
-    creator: req.token.id,
-  });
-
-  newTodo
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+    const newTodo = new tasksModel({
+      name,
+      creator: req.token.id,
     });
+
+    newTodo
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(404).json({ message: "This user is deleted" });
+  }
 };
 
 const updateTodo = (req, res) => {
